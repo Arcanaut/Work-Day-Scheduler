a865b3bdebf589705cdc3f12608df83324e6bef3
Brendan Ahearn <91020994+Arcanaut@users.noreply.github.com>
2021-11-20 17:04:26 -0600

    upload for AskBCs help

--- Develop/script.js
+++ Develop/script.js
@@ -9,19 +9,29 @@ var currentHour = parseInt(moment().format('H'));
 
 //changes background-color according to time
 //tB refers to the timeBlock
-$(".timeblock").each(function(){
+$(".timeblock").each(function () {
     var tB = $(this).attr("id")
     console.log(tB);
-if (tB == currentHour ) {
-    
-    $(this).children("textarea").addClass("present");
-}
-else if(tB < currentHour)
-{
-    $(this).children("textarea").addClass("past");
+    if (tB == currentHour) {
 
-}
-else if(tB >currentHour ){
-    $(this).children("textarea").addClass("future");
-}
+        $(this).children("textarea").addClass("present");
+    } else if (tB < currentHour) {
+        $(this).children("textarea").addClass("past");
+
+    } else if (tB > currentHour) {
+        $(this).children("textarea").addClass("future");
+    }
 });
+
+var textboxArea = $(this).siblings(".hour").children("textarea").val();
+var textboxID = $(this).siblings(".hour").attr("id");
+
+var saveSchedule = function () {
+    console.log($(this).siblings(".hour").children("textarea").val());
+    var textboxArea = $(this).siblings(".hour").children("textarea").val();
+    var textboxID = $(this).siblings(".hour").attr("id");
+    localStorage.setItem(textBoxID, textBoxArea);
+}
+
+//save the text fields when save button is clicked
+$(".saveBtn").on("click", saveSchedule)
