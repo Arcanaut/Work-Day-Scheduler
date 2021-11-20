e0b67fc96c4b9ce2aa61410ca9fb5274ea474746
Brendan Ahearn <91020994+Arcanaut@users.noreply.github.com>
2021-11-20 14:27:29 -0600

    Fixed cols

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,27 @@
-const m = moment();
-console.log(m.toString());
+document.querySelector("#currentDay");
+var m = moment().format("dddd MMMM, Do");
 
-m.format("dddd");
+//gives the inline 
+$("#currentDay").text(m);
+
+var currentHour = (moment().format('H'));
+
+
+//changes background-color according to time
+//tB refers to the timeBlock
+$(".timeblock").each(function(){
+    var tB = $(this).attr("id")
+    console.log(tB);
+if (tB === currentHour ) {
+    
+    $(this).children("textarea").addClass("present");
+}
+else if(tB < currentHour)
+{
+    $(this).children("textarea").addClass("past");
+
+}
+else if(tB >currentHour ){
+    $(this).children("textarea").addClass("future");
+}
+});
