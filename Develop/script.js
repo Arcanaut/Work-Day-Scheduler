017ed1984826f81f8f80dc8144143342a3536e0c
Brendan Ahearn <91020994+Arcanaut@users.noreply.github.com>
2021-11-20 20:13:09 -0600

    fixed local storage, broke css

--- Develop/script.js
+++ Develop/script.js
@@ -11,7 +11,6 @@ var currentHour = parseInt(moment().format('H'));
 //tB refers to the timeBlock
 $(".timeblock").each(function () {
     var tB = $(this).attr("id")
-    console.log(tB);
     if (tB == currentHour) {
 
         $(this).children("textarea").addClass("present");
@@ -23,15 +22,31 @@ $(".timeblock").each(function () {
     }
 });
 
-var textboxArea = $(this).siblings(".hour").children("textarea").val();
-var textboxID = $(this).siblings(".hour").attr("id");
 
-var saveSchedule = function () {
-    console.log($(this).siblings(".hour").children("textarea").val());
-    var textboxArea = $(this).siblings(".hour").children("textarea").val();
-    var textboxID = $(this).siblings(".hour").attr("id");
+saveSchedule = function () { 
+    var textBoxArea = $(this).siblings("textarea").val();
+    var textBoxID = $(this).parent().attr("id")
     localStorage.setItem(textBoxID, textBoxArea);
 }
 
+
+
+
 //save the text fields when save button is clicked
 $(".saveBtn").on("click", saveSchedule)
+
+// var textBoxArea = $(this).siblings(".hour").children("textarea").val();
+// var textBoxID = $(this).siblings(".hour").attr("id");
+// localStorage.setItem(textBoxID, textBoxArea);
+
+//description class, 
+
+$("#hour9").children("textarea").val(localStorage.getItem("hour9"));
+$("#hour10").children("textarea").val(localStorage.getItem("hour10"));
+$("#hour11").children("textarea").val(localStorage.getItem("hour11"));
+$("#hour12").children("textarea").val(localStorage.getItem("hour12"));
+$("#hour13").children("textarea").val(localStorage.getItem("hour13"));
+$("#hour14").children("textarea").val(localStorage.getItem("hour14"));
+$("#hour15").children("textarea").val(localStorage.getItem("hour15"));
+$("#hour16").children("textarea").val(localStorage.getItem("hour16"));
+$("#hour17").children("textarea").val(localStorage.getItem("hour17"));
